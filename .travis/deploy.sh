2610a748750e83da30419ce0d4bf390f88d67a4f
Joshua Payne <suxur@me.com>
2018-03-21 23:06:11 -0500

    Update remote deployment url

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -10,7 +10,7 @@ if [ $TRAVIS_BRANCH == "develop" ] ; then
     eval "$(ssh-agent -s)"
     chmod 600 travis_rsa
     ssh-add travis_rsa
-    git remote add deploy "travis@138.68.27.217:/var/www/develop.reguluswallet.com"
+    git remote add deploy "travis@138.68.27.217:/var/www/develop.reguluswallet.com.git"
     git config user.name "suxur"
     git config user.email "suxur@me.com"
 
